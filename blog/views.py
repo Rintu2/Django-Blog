495699cdc957e6d05cef70c93cf58d93c6b513e6
Rintu <er.rintu@gmail.com>
2020-03-06 18:35:57 +0530

    User Profile Creation

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Post
+
 # posts = [
 #     {
 #         'author': 'Rintu',
