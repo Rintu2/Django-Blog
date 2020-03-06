751286e8075ecf6de420c6848d1c020aa4b6b886
Rintu <er.rintu@gmail.com>
2020-03-06 12:03:00 +0530

    login/logout

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
     path('', include('blog.urls')),
 
